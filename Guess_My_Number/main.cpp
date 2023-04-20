6f59f44217bed95351225a4d80e5bac4840df238
jonglass82 <jonglass82@gmail.com>
2023-04-19 19:07:17 -0500

    Initial Commit

---
+++ Guess_My_Number/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Guess_My_Number
+//
+//  Created by jon glass on 4/19/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
