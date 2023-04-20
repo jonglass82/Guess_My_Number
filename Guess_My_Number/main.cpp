0b63ba23ad998ee1fcafda0245df9f0973a52904
jonglass82 <jonglass82@gmail.com>
2023-04-19 20:28:22 -0500

    initial commit

--- Guess_My_Number/main.cpp
+++ Guess_My_Number/main.cpp
@@ -8,7 +8,24 @@
 #include <iostream>
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int num = 1;
+    int userGuess;
+    
+    do {
+        std::cout << "Pick a number 1-10...";
+        std::cin >> userGuess;
+
+        if(userGuess < 1 || userGuess > 10){
+            std::cout << "Your guess has to be a number between 1-10. Guess again...";
+        }
+        else{
+            std::cout << "NOPE. You guessed: " << userGuess << "Guess again";
+        }
+    }while(userGuess != num);
+
+
+    std::cout << "CORRECT!!! the number is: " << num;
+
+    
     return 0;
 }
